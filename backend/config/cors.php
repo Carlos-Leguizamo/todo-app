6409269d7a2dfbe4c314034b239c96e0becc4378
Carlos Leguizamo <tato2005leguizamo@gamil.com>
2024-10-09 12:30:38 -0500

    Back

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://todo-app-vert-nu.vercel.app'], // Cambia a tu dominio específico
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, 
 
 ];
